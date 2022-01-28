c6c46b40e8714ec817cf605a60322842d6eda209
UTKARSHBOTS <90956407+utkarsh212646@users.noreply.github.com>
2022-01-28 10:26:28 +0530

    Update __init__.py

--- Yuriko/__init__.py
+++ Yuriko/__init__.py
@@ -282,7 +282,7 @@ DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 
 # Load at end to ensure all prev variables have been set
-from Yuriko.modules.helper_funcs.handlers import (
+from minori.modules.helper_funcs.handlers import (
     CustomCommandHandler,
     CustomMessageHandler,
     CustomRegexHandler,
